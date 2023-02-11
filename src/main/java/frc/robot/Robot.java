791e8e80e364bea619ba888696aaa887caf3023e
James <igknightion@gmail.com>
2023-02-11 10:20:02 -0600

    brug lets see what happens

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -136,6 +136,15 @@ double armSpeed = 0; //speed and direction of the arm
     }else{
       armSpeed=0;
     }
+    //second controller arm overwrite (Right and left triggers)
+    if(armCntrl.getRightTriggerAxis()>0 && armHeight<4){
+      armSpeed=armCntrl.getRightTriggerAxis();
+    }else{
+      armSpeed=0;
+    }
+    if(armCntrl.getLeftTriggerAxis()>0 && armHeight>0){
+      armSpeed=-armCntrl.getLeftTriggerAxis();
+    }
   }
 
 
