cc64e979136338fb854e6442df56b9e219d683c5
James <igknightion@gmail.com>
2023-02-11 09:37:45 -0600

    started changing from 4 to 1 limit switch

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -27,15 +27,14 @@ public class Robot extends TimedRobot {
   //this is the arm motor, device number probaly will have to change, it is also the only motor used in this test
 WPI_TalonSRX arm = new WPI_TalonSRX(11);
   //Controllers, current plan is to have two drivers, one solely for drive and one for the arm
-XboxController driveCntrl = new XboxController (1);
-XboxController armCntrl = new XboxController (2);
+XboxController driveCntrl = new XboxController (1); //primary driver
+XboxController armCntrl = new XboxController (2); //primary arm mover, has overrides
   //Limit Switches, resting is all the way down, and one for each height we can score at
-DigitalInput restingLimit = new DigitalInput(0);
-DigitalInput lowLimit = new DigitalInput (1);
-DigitalInput midLimit = new DigitalInput (2);
-DigitalInput highLimit = new DigitalInput (3);
-int armHeight = 0; //based on what limit switch was last pressed, we know where the arm is and what direction it needs to go to get to its next spot.
+DigitalInput armDetector = new DigitalInput(0);
+
+int armHeight = 0; //decides where the arm is based on when the limit switch has been pressed
   
+double armSpeed = 0; //speed and direction of the arm
 /* This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
@@ -44,6 +43,7 @@ int armHeight = 0; //based on what limit switch was last pressed, we know where 
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+
   }
 
   /**
@@ -54,8 +54,17 @@ int armHeight = 0; //based on what limit switch was last pressed, we know where 
    * SmartDashboard integrated updating.
    */
   @Override
-  public void robotPeriodic() {}
+  public void robotPeriodic() {
+    //changes arm height if limit switch is pressed
+    if (armSpeed > 0 && armDetector.get() == true){
+      armHeight ++;
+    } else if (armSpeed<0 && armDetector.get() == true){
+      armHeight --;
+    }
+  }
 
+
+  
   /**
    * This autonomous (along with the chooser code above) shows how to select between different
    * autonomous modes using the dashboard. The sendable chooser code works with the Java
@@ -94,56 +103,12 @@ int armHeight = 0; //based on what limit switch was last pressed, we know where 
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    //sets the arm height to the last activated limit switch so we know which direction it needs to move
-    if (restingLimit.get()==true){
-      armHeight = 0;
-    } else if (lowLimit.get()==true){
-      armHeight = 1;
-    } else if (midLimit.get()==true){
-      armHeight = 2;
-    } else if (highLimit.get()==true){
-      armHeight = 3;
-    }
-    //Quick adjust for arm speeds up and down
-    double armSpeedUp = .15;
-    double armSpeedDown = -.15;
-   //pressing B brings the motor back down to where it starts
-    if(armCntrl.getBButton()==true && restingLimit.get() == false){
-      arm.set(armSpeedDown);
+  
+    //B brings the arm to rest BOTH CONTROLLERS
+    if ((armCntrl.getBButton() == true || driveCntrl.getBButton() == true) && armHeight > 0){
+      armSpeed = .15;
     } else {
-      arm.set(0);
-    }
-    //pressing A brings the motor to lowest scoring position
-    if (armCntrl.getAButton()==true && lowLimit.get()==false && armHeight<1){
-      arm.set(armSpeedUp);
-    }else if (armCntrl.getAButton()==true && lowLimit.get()==false && armHeight>1){
-      arm.set(armSpeedDown);
-    }else {arm.set (0);
-    }
-    //pressing X brings motor to middle scoring position
-    if(armCntrl.getXButton()==true && midLimit.get()==false && armHeight<2){
-      arm.set(armSpeedUp);
-    }else if (armCntrl.getXButton()==true && midLimit.get()==false && armHeight>2){
-      arm.set(armSpeedDown);
-    }else {
-      arm.set(0);
-    }
-    //pressing Y brings motor to highest scoring position
-    if(armCntrl.getYButton()==true && highLimit.get()==false){
-      arm.set(armSpeedUp);
-    }else {arm.set(0);
-    }
-    //Right bumper moves arm up regardless of position as an override
-    if(armCntrl.getRightBumper()==true){
-      arm.set(armSpeedUp);
-    }else {
-      arm.set(0);
-    }
-    //left bumper moves arm down regardless of position as an override
-    if(armCntrl.getLeftBumper()==true) {
-      arm.set(armSpeedDown);
-    } else{
-      arm.set(0);
+      armSpeed = 0;
     }
   }
 
