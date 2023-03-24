d53d6a32c5fdca84f063495a5b34ddabf52a9c64
James <igknightion@gmail.com>
2023-03-24 07:07:05 -0500

    good code

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -6,6 +6,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.sensors.SensorInitializationStrategy;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
@@ -27,65 +28,69 @@ public class ArmSubsystem extends SubsystemBase {
         m_armMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
         m_armMotor.setNeutralMode(NeutralMode.Brake);
         m_armMotor.configFeedbackNotContinuous(false, 10);
-        m_armMotor.config_kP(0, 0);
+        m_armMotor.config_kP(0, 0.0035);
         m_armMotor.config_kI(0, 0);
-        m_armMotor.config_kD(0, 0);
+        m_armMotor.config_kD(0, 0.1);
         m_armMotor.config_kF(0, 0);
+        m_armMotor.configIntegratedSensorInitializationStrategy(SensorInitializationStrategy.BootToAbsolutePosition);
+        m_armMotor.configPeakOutputForward(.3);
+        m_armMotor.configPeakOutputReverse(.3);
+
 
         m_wristMotor.configFactoryDefault();
         m_wristMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
         m_wristMotor.setNeutralMode(NeutralMode.Brake);
         m_wristMotor.configFeedbackNotContinuous(false, 10);
-        m_wristMotor.config_kP(0, 0);
+        m_wristMotor.config_kP(0, 0.1);
         m_wristMotor.config_kI(0, 0);
         m_wristMotor.config_kD(0, 0);
         m_wristMotor.config_kF(0, 0);
+        m_wristMotor.configIntegratedSensorInitializationStrategy(SensorInitializationStrategy.BootToAbsolutePosition);
+        m_wristMotor.configPeakOutputForward(.3);
+        m_wristMotor.configPeakOutputReverse(.3);
     }
     //checks for arm resting limit switch, defaults everything to the override if it is being used
     public void isRestingPosition (double lTSecondary, double rTSecondary, double armOverride, double deadband) {
-        if (m_armPointZero.get() == true){
-            m_armMotor.setSelectedSensorPosition(0);
-        }
+        // if (m_armPointZero.get() == false){
+        //     m_armMotor.setSelectedSensorPosition(0);
+        // }
         // if (m_wristPointZero.get() == true){
         //     m_wristMotor.setSelectedSensorPosition(0);
         // }
         m_armMotor.set(TalonFXControlMode.PercentOutput, MathUtil.applyDeadband(armOverride, deadband));
         m_wristMotor.set(TalonFXControlMode.PercentOutput, (rTSecondary - lTSecondary));
-        SmartDashboard.putBoolean("wristLimit", m_wristPointZero.get());
-        SmartDashboard.putNumber("wristDist", m_wristMotor.getSelectedSensorPosition());
-        SmartDashboard.putBoolean("armLimit", m_armPointZero.get());
     }
     //B on controller 1, arm to rest
     public void setRestingPosition(){
-        m_armMotor.set(TalonFXControlMode.Position, 0);
-        m_wristMotor.set(TalonFXControlMode.Position, 0);
+        m_armMotor.set(TalonFXControlMode.Position, 50000);
+        m_wristMotor.set(TalonFXControlMode.Position, -70000);
     }
     //A on controller 2, Arm to low score/pickup position
     public void armPositionOne(){
-        m_armMotor.set(TalonFXControlMode.Position, 1);
-        m_wristMotor.set(TalonFXControlMode.Position, -1);
+        m_armMotor.set(TalonFXControlMode.Position, -40000);
+        m_wristMotor.set(TalonFXControlMode.Position, -70000);
     }
     //B on controller 2, arm to mid scoring position
     public void armPositionTwo(){
-        m_armMotor.set(TalonFXControlMode.Position, 2);
-        m_wristMotor.set(TalonFXControlMode.Position, 0);
+        m_armMotor.set(TalonFXControlMode.Position, -180000);
+        m_wristMotor.set(TalonFXControlMode.Position, -70000);
     }
     //X on controller 2, arm to slide pickup position
     public void armPositionFour(){
-        m_armMotor.set(TalonFXControlMode.Position, 4);
-        m_wristMotor.set(TalonFXControlMode.Position, 0);
+        m_armMotor.set(TalonFXControlMode.Position, -180000);
+        m_wristMotor.set(TalonFXControlMode.Position, -70000);
     }
     //Y on controller 2, arm to high scoring position
     public void armPositionThree(){
-        m_armMotor.set(TalonFXControlMode.Position, 3);
-        m_wristMotor.set(TalonFXControlMode.Position, 0);
+        m_armMotor.set(TalonFXControlMode.Position, -200000);
+        m_wristMotor.set(TalonFXControlMode.Position, -40000);
     }
 
     public void wristPositionDownPickup(){
-        m_wristMotor.set(TalonFXControlMode.Position, -2);
+        m_wristMotor.set(TalonFXControlMode.Position, 0);
     }
     
     public void wristPositionPickup(){
-        m_wristMotor.set(TalonFXControlMode.Position, -1);
+        m_wristMotor.set(TalonFXControlMode.Position, -40000);
     }
 }
